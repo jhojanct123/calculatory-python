1a72018499fad1e30388263522574f98116a4e11
jhojanct123 <jhojan.ct2006@gmail.com>
2025-09-18 07:05:55 -1200

    Add files via upload

---
+++ MI PRIMER PROYECTO.py
@@ -0,0 +1,11 @@
+def Calculadora
+print ("Calculadora de Stiven")
+print ("Operaciones disponibles")
+print (" 1. Suma")
+print (" 2. Resta")
+print (" 3. Multiplicacion")
+print (" 4. Division")
+print (" 5. Salir")
+
+
+
